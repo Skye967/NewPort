566c4a6aebd6474d0634756715f8e58c8d1228a2
skyonide <skye.grossman@gmail.com>
2024-07-01 17:17:16 -0600

    meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Skye Grossman",
   description: "Skye Grossman's work portfolio",
+  
 };
 
-<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 export default function RootLayout({
   children,
@@ -18,6 +19,9 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="scroll-smooth w-full">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
         <body className="scroll-smooth w-full">
           {children}
         </body>
