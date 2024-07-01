15fd5873284a0ad73cd06350d6be2b46b4cab068
skyonide <skye.grossman@gmail.com>
2024-07-01 14:22:22 -0600

    some css format changes

--- app/page.tsx
+++ app/page.tsx
@@ -44,16 +44,16 @@ const HomePage: React.FC = () => {
             <h1 className={styles.aboutMeHeader}>About Me</h1>
             <h2 className={styles.subHeader}>Who I am and what I do</h2>
           </div>
-          <div className={styles.flexWrap} >
-            <div className="m-10  min-w-[400px]">
+          <div className={`${styles.flexWrap} `} >
+            <div className="m-10  min-w-[350px] flex flex-col justify-center items-center ">
               <h3 className={styles.introduction}>
                 Hello, I'm <span>S</span>kye <span>Grossman</span>
               </h3>
               <div className={styles.biographyContainer}>
-                <span className={styles.biography}><span className={styles.tab}></span> I come from the island Maui, Hawaii, and I frequently enjoy cooking, fitness, and training Jiu-Jitsu. I had a fun upbringing growing up in the country side which involved caring for farm animals, building structures, and fixing equipment. When we weren't working we where fishing, riding our motorbikes, and we used to skip school when the surf was to good to pass up. <br></br> <span className={styles.tab}></span>My adult journey started with culinary school, where I earned my degree. The restaurant industry was great and full of fun, from working in bars and dives to catering and upscale restaurants. After some time, an opportunity in the towing industry came my way, and I took it. This phase of my life was full of unique experiences, from repossessions to off-road tow-outs and vehicle removals, all kinds of crazy situations. <br></br> <span className={styles.tab}></span>Eventually after enough tough situations, I sought a new path and turned to construction, where I gained valuable skills. However, I realized that it wasn't where I wanted my future. I discovered I had a keen interest in programming, having fun in creating and seeing technology come to life. To refine my skills, I attended an intensive 16-week coding bootcamp at Coding Dojo, coding 12 to 15 hours a day. This rigorous training whipped me into coding shape.<br></br> <span className={styles.tab}></span>Now, I have the privilege of working on and creating amazing software. Where I get to employ my own personal philosophy to continually improve, and innovate.<span className={styles.bioRed}>If you have any questions or just want to chat, feel free to get in touch.</span></span>
+                <span className={styles.biography}><span className={styles.tab}></span> I come from the island Maui, Hawaii, and I frequently enjoy cooking, fitness, and training Jiu-Jitsu. I had a fun upbringing growing up on the island's country side which involved caring for farm animals, building structures, fixing and equipment. When we weren't working we where fishing, riding our motorbikes, and we used to skip school when the surf was to good to pass up. <br></br> <span className={styles.tab}></span>My adult journey started with culinary school, where I earned my degree. The restaurant industry was great and full of fun, from working in bars and dives to catering and upscale restaurants. After some time, an opportunity in the towing industry came my way, and I took it. This phase of my life was full of unique experiences, from repossessions to off-road tow-outs and vehicle removals, all kinds of crazy situations. <br></br> <span className={styles.tab}></span>Eventually after enough tough situations, I sought a new path and turned to construction, where I gained valuable skills. However, I realized that it wasn't where I wanted my future. I discovered I had a keen interest in programming, having fun in creating and seeing technology come to life. To refine my skills, I attended an intensive 16-week coding bootcamp at Coding Dojo, coding 12 to 15 hours a day. This rigorous training whipped me into coding shape.<br></br> <span className={styles.tab}></span>Now, I have the privilege of working on and creating amazing software. Where I get to employ my own personal philosophy to continually improve, and innovate.<span className={styles.bioRed}>If you have any questions or just want to chat, feel free to get in touch.</span></span>
               </div>
             </div>
-            <div className="flex justify-center items-center flex-col">
+            <div className="w-full flex justify-center items-start flex-col">
               <h4 className={styles.toolsHeader}>Tools and Technologies I'm familiar with:</h4>
               <div className={styles.toolsGrid}>
                 {logoImages.map((tool, index) => (
@@ -84,6 +84,7 @@ const HomePage: React.FC = () => {
           <video className={styles.videoBackground} controls={false} autoPlay={true} loop={true} muted={true} preload="none" playsInline>
             <source src={"/video/milkyway.mp4"} type="video/mp4" />
           </video>
+          <h2 className={styles.projectsHeader}>My Recent Work</h2>
           <Image
             src={desktop}
             alt="Description of image"
@@ -91,7 +92,6 @@ const HomePage: React.FC = () => {
         </div>
       </section>
       <section id="projects" className={styles.projectsSection}>
-        <h2 className={styles.projectsHeader}>Recent Projects</h2>
         <ProjectCard
           imageSrc={project1.imageSrc}
           description={project1.description}
