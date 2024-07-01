b05fd050cfcfb7c357233768fecbaaa101a22265
skyonide <skye.grossman@gmail.com>
2024-07-01 14:37:13 -0600

    changed project descriptions

--- app/constants/projects.tsx
+++ app/constants/projects.tsx
@@ -26,7 +26,7 @@ import kalaniCommerce from '@/public/port-images/KalaniShop.png'
 
 export const project1 = {
     imageSrc: pocketChef,
-    description: 'PocketChef is an innovative web application designed to revolutionize the way you approach cooking. Built with a modern tech stack including TypeScript, React.js, Next.js, TailwindCSS, CSS, and OpenAI, PocketChef provides a seamless and interactive user experience. Simply enter the ingredients you have or wish to use, optionally select a diet type and meal type, and PocketChef will generate a variety of recipes complete with images. The app leverages the power of OpenAI to create unique and delicious recipes tailored to your preferences. Additionally, PocketChef showcases advanced parallax effects and GSAP animations, making the user interface visually appealing and engaging. Whether you are a home cook or a culinary enthusiast, PocketChef is your ultimate kitchen companion, helping you make the most out of your ingredients and discover new culinary delights.',
+    description: 'PocketChef is an innovative way you approach cooking, solving the issue of thinking of something to cook with the limited ingredients inside your kitchen. Built with a modern tech stack including TypeScript, React.js, Next.js, TailwindCSS, CSS, and OpenAI. Simply enter the ingredients you have or wish to use, optionally select a diet type and meal type, and PocketChef will generate a variety of recipes complete with images. The app leverages the power of OpenAI to create unique and delicious recipes tailored to your preferences. Additionally, PocketChef showcases advanced parallax effects and GSAP animations, making the user interface more appealing and engaging. Built this to help the average home cook, PocketChef is your ultimate kitchen companion, helping you make the most out of your ingredients and discover interesting recipes from your own kitchen.',
     title: 'PocketChef',
     tools: [
         {name: 'Javascript', image: javascript},
@@ -43,7 +43,7 @@ export const project1 = {
 
 export const project2 = {
     imageSrc: kalaniCommerce,
-    description: 'This e-commerce web application offers a comprehensive shopping experience. Users can effortlessly log in using GoogleOauth, ensuring a secure and seamless authentication process. The app features robust cart and checkout functionality, enabling users to manage their shopping with ease. Additionally, it includes a dynamic dashboard for users to track their orders and manage their account details. The integration with Stripe ensures smooth and secure payment transactions, enhancing the overall user experience. This e-commerce solution is designed to provide a modern, efficient, and user-friendly platform for both buyers and sellers.',
+    description: 'This e-commerce web application offers a smooth comprehensive shopping experience. Showcasing a modern approach to api and database techniques, reducing development time by half without sacrificing quality. Users can effortlessly log in using GoogleOauth, ensuring a secure and seamless authentication process. The app features robust cart and checkout functionality. Additionally, it includes a dynamic dashboard for users to track their orders and manage their account details. The integration with Stripe ensures smooth and secure payment transactions. This e-commerce solution is designed to provide a modern, efficient, and user-friendly platform for both buyers and sellers.',
     title: 'Kalani-Commerce',
     tools: [
         {name: 'Javascript', image: javascript},
